d4239fb888bfff5a52cefd2306ecdeba9b9af611
Eric Smith <eric@8thlight.com>
2010-09-16 23:44:44 -0500

    Semi-solved.  This should work, but maybe I'm not using audacity right.

--- karoke/karoke.cpp
+++ karoke/karoke.cpp
@@ -194,9 +194,9 @@ OSStatus karoke::ProcessBufferLists	(AudioUnitRenderActionFlags&	iFlags,
 
   while (iFrames > 0)
   {
-    *leftOut = *leftSample;
-    *rightOut = *rightSample;
-
+    *leftOut = *leftSample - *rightSample;
+    *rightOut = *rightSample - *leftSample;
+    
     iFrames--;
     leftSample++;
     rightSample++;
